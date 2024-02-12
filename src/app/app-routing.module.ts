bb0b6cec5d3813687ea553bebd0669c13e57a83c
João Victor Correa <correa.joaov25@gmail.com>
2024-02-12 16:01:23 -0300

    Organized Folders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { ProductsComponent } from './components/products/products.component';
-import { CategoriesComponent } from './categories/categories.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
